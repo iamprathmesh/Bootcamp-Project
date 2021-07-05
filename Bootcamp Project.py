2c71c03bb0e60defed8b537931beda3bde577387
iamprathmesh <86964450+iamprathmesh@users.noreply.github.com>
2021-07-05 19:06:13 +0530

    Add files via upload

---
+++ Bootcamp Project.py
@@ -0,0 +1,14 @@
+import hashlib
+import random
+
+user_input = input("Enter your password: ")
+str_ = ""
+for i in range(1, len(user_input)+1):
+    num = random.randint(1, 100)
+    if i % 3 == 0:
+        if num % 2 == 0:
+            str_ = user_input[:i] + str(num) + user_input[i:]
+
+print(str_)
+message = str_.encode()
+print("MD5:", hashlib.md5(message).hexdigest())
