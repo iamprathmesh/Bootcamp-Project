2ce445811152b6f37abc89d414c3bcf934026e4f
Prathmesh Bhandare <86964450+iamprathmesh@users.noreply.github.com>
2024-01-07 20:24:57 +0530

    Update Bootcamp Project.py

--- Bootcamp Project.py
+++ Bootcamp Project.py
@@ -8,7 +8,6 @@ for i in range(1, len(user_input)+1):
     if i % 3 == 0:
         if num % 2 == 0:
             str_ = user_input[:i] + str(num) + user_input[i:]
-
 print(str_)
 message = str_.encode()
 print("MD5:", hashlib.md5(message).hexdigest())
